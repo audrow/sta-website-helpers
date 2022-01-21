f4a14e5bea0e365312c53f4a366daa40ca071ffc
Audrow Nash <audrow@hey.com>
2022-01-21 07:55:18 -0800

    Initial commit
    Signed-off-by: Audrow Nash <audrow@hey.com>

---
+++ src/sum.ts
@@ -0,0 +1,10 @@
+import { Podcast } from "podcast";
+
+const _feed = new Podcast({
+  title: "My Awesome Podcast",
+  description: "This is my awesome podcast",
+})
+
+export function sum(a: number, b: number): number {
+  return a + b;
+}
