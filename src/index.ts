e2f49eac3e8c23161c0d926b0360781b3b209cd0
Audrow Nash <audrow@hey.com>
2022-01-23 11:06:45 -0800

    Export types
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 export { sum } from './sum';
+
+import type EpisodeYamlData from './types/EpisodeYamlData';
+import type EpisodeData from './types/EpisodeData';
+import type Link from './types/Link';
+import type PodcastConfig from './types/PodcastConfig';
+
+export type { EpisodeYamlData, EpisodeData, Link, PodcastConfig };
